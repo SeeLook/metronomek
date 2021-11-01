9dc78ac6c27ed18fc88deff712b5c48338c1b3e3
SeeLook <seelook@gmail.com>
2021-11-01 18:01:34 +0100

    Adjust CMake rules for Android, update JNI calls

--- src/android/tandroid.cpp
+++ src/android/tandroid.cpp
@@ -1,21 +1,21 @@
 /** This file is part of Metronomek                                  *
- * Copyright (C) 2020 by Tomasz Bojczuk (seelook@gmail.com)          *
+ * Copyright (C) 2020-2021 by Tomasz Bojczuk (seelook@gmail.com)     *
  * on the terms of GNU GPLv3 license (http://www.gnu.org/licenses)   */
 
 
 #include "tandroid.h"
-#include <QtAndroidExtras/qandroidfunctions.h>
-#include <QtAndroidExtras/qandroidjnienvironment.h>
 
+#include <QtCore/qcoreapplication.h>
+#include <QtCore/qjniobject.h>
 #include <QtCore/qdebug.h>
 
 
 void Tandroid::keepScreenOn(bool on) {
-  QtAndroid::runOnAndroidThread([on]{
-    QAndroidJniObject activity = QtAndroid::androidActivity();
+  QNativeInterface::QAndroidApplication::runOnAndroidMainThread([on]{
+    QJniObject activity = QNativeInterface::QAndroidApplication::context();
     if (activity.isValid()) {
-      QAndroidJniObject window =
-          activity.callObjectMethod("getWindow", "()Landroid/view/Window;");
+      QJniEnvironment env;
+      QJniObject window = activity.callObjectMethod("getWindow", "()Landroid/view/Window;");
 
       if (window.isValid()) {
         const int FLAG_KEEP_SCREEN_ON = 128;
@@ -25,25 +25,19 @@ void Tandroid::keepScreenOn(bool on) {
           window.callMethod<void>("clearFlags", "(I)V", FLAG_KEEP_SCREEN_ON);
       }
     }
-    QAndroidJniEnvironment env;
-    if (env->ExceptionCheck())
-      env->ExceptionClear();
   });
 }
 
 
 void Tandroid::disableRotation(bool disRot) {
   int orientation = disRot ? 1 : 10; // SCREEN_ORIENTATION_PORTRAIT or SCREEN_ORIENTATION_FULL_SENSOR
-  QtAndroid::runOnAndroidThread([orientation]{
-    QAndroidJniObject activity = QtAndroid::androidActivity();
+  QNativeInterface::QAndroidApplication::runOnAndroidMainThread([orientation]{
+    QJniObject activity = QNativeInterface::QAndroidApplication::context();
     if (activity.isValid())
       activity.callMethod<void>("setRequestedOrientation" , "(I)V", orientation);
-    QAndroidJniEnvironment env;
-    if (env->ExceptionCheck())
-      env->ExceptionClear();
   });
 }
 
 int Tandroid::getAPIlevelNr() {
-  return  QtAndroid::androidSdkVersion();
+  return  QNativeInterface::QAndroidApplication::sdkVersion();
 }
