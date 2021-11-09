defd275e9e056a0b0407791f406a610f8d7a0991
SeeLook <seelook@gmail.com>
2021-11-09 21:53:19 +0100

    Improve doc

--- src/tabstractaudiooutput.h
+++ src/tabstractaudiooutput.h
@@ -58,7 +58,9 @@ signals:
        * @p unsigned::int& reference is returning value and depends on audio back-end:
        * For @p RtAudio it is callback return value (0 - continue, 1, 2 - to stop).
        * In @p Qt::Audio it is how many data frames were processed.
-       * In any case: @p TaudioOUT::outCallBack has to be aware of its meaning
+       * In any case: @p TaudioOUT::outCallBack has to be aware of its meaning.
+       * WARNING: This signal has to be connected using @p Qt::Qt::DirectConnection
+       * to perform front-end callback routines in back-end thread.
        */
   void feedAudio(char*, unsigned int, unsigned int&);
 
