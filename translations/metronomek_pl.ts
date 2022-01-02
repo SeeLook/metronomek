af430a9f04c458561ee9159a9519a8863fb51ed7
SeeLook <seelook@gmail.com>
2022-01-02 13:53:33 +0100

    Updated Polish translation

--- translations/metronomek_pl.ts
+++ translations/metronomek_pl.ts
@@ -4,67 +4,67 @@
 <context>
     <name>BeatType</name>
     <message>
-        <location filename="../src/taudioout.cpp" line="304"/>
+        <location filename="../src/taudioout.cpp" line="347"/>
         <source>Metronome beat</source>
         <translation>Tykanie metronomu</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="304"/>
+        <location filename="../src/taudioout.cpp" line="347"/>
         <source>Metronome beat 2</source>
         <translation>Tykanie metronomu 2</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="305"/>
+        <location filename="../src/taudioout.cpp" line="348"/>
         <source>Snapping fingers</source>
         <translation>Pstrykanie palcami</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="305"/>
+        <location filename="../src/taudioout.cpp" line="348"/>
         <source>Beating at parapet</source>
         <translation>Stukanie w parapet</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="306"/>
+        <location filename="../src/taudioout.cpp" line="349"/>
         <source>Drum sticks</source>
         <translation>Pałeczki perkusyjne</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="306"/>
+        <location filename="../src/taudioout.cpp" line="349"/>
         <source>Drum sticks 2</source>
         <translation>Pałeczki perkusyjne 2</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="307"/>
+        <location filename="../src/taudioout.cpp" line="350"/>
         <source>Clapping</source>
         <translation>Klaskanie</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="307"/>
+        <location filename="../src/taudioout.cpp" line="350"/>
         <source>Guitar body</source>
         <translation>Pudło gitary</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="308"/>
+        <location filename="../src/taudioout.cpp" line="351"/>
         <source>Drum 1</source>
         <translation>Bęben 1</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="308"/>
+        <location filename="../src/taudioout.cpp" line="351"/>
         <source>Drum 2</source>
         <translation>Bęben 2</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="308"/>
+        <location filename="../src/taudioout.cpp" line="351"/>
         <source>Drum 3</source>
         <translation>Bęben 3</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="309"/>
+        <location filename="../src/taudioout.cpp" line="352"/>
         <source>Base Drum</source>
         <translation>Baza</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="309"/>
+        <location filename="../src/taudioout.cpp" line="352"/>
         <source>Snare drum</source>
         <translation>Werbel</translation>
     </message>
@@ -72,12 +72,12 @@
 <context>
     <name>InfoPage</name>
     <message>
-        <location filename="../src/qml/InfoPage.qml" line="50"/>
+        <location filename="../src/qml/InfoPage.qml" line="58"/>
         <source>Metronomek ticks and rings through&lt;br&gt;&lt;b&gt;%1&lt;/b&gt; library.</source>
         <translation>Metronomek stuka i dzwoni przez bibliotekę&lt;br&gt;&lt;b&gt;%1&lt;/b&gt;.</translation>
     </message>
     <message>
-        <location filename="../src/qml/InfoPage.qml" line="62"/>
+        <location filename="../src/qml/InfoPage.qml" line="70"/>
         <source>This program is free software; you can redistribute it and/or modify
           it under the terms of the GNU General Public License as published by
           the Free Software Foundation; either version 3 of the License, or
@@ -110,7 +110,7 @@ Place, Fifth Floor, Boston, MA  02110-1301  USA</translation>
 <context>
     <name>Logo</name>
     <message>
-        <location filename="../src/qml/Logo.qml" line="47"/>
+        <location filename="../src/qml/Logo.qml" line="52"/>
         <source>The rhythmic&lt;br&gt;perfection</source>
         <translation>Rytmiczna&lt;br&gt;doskonałość</translation>
     </message>
@@ -124,32 +124,32 @@ Place, Fifth Floor, Boston, MA  02110-1301  USA</translation>
     </message>
     <message>
         <location filename="../src/qml/MainDrawerContent.qml" line="47"/>
-        <location filename="../src/qml/MainDrawerContent.qml" line="73"/>
+        <location filename="../src/qml/MainDrawerContent.qml" line="66"/>
         <source>ring at &quot;one&quot;</source>
         <translation>dzwoń na &quot;raz&quot;</translation>
     </message>
     <message>
-        <location filename="../src/qml/MainDrawerContent.qml" line="66"/>
+        <location filename="../src/qml/MainDrawerContent.qml" line="74"/>
         <source>count down visible</source>
         <translation>wyświetlaj odliczanie</translation>
     </message>
     <message>
-        <location filename="../src/qml/MainDrawerContent.qml" line="80"/>
+        <location filename="../src/qml/MainDrawerContent.qml" line="82"/>
         <source>pendulum stationary</source>
         <translation>nieruchome wahadło</translation>
     </message>
     <message>
-        <location filename="../src/qml/MainDrawerContent.qml" line="87"/>
+        <location filename="../src/qml/MainDrawerContent.qml" line="102"/>
         <source>settings</source>
         <translation>ustawienia</translation>
     </message>
     <message>
-        <location filename="../src/qml/MainDrawerContent.qml" line="97"/>
+        <location filename="../src/qml/MainDrawerContent.qml" line="111"/>
         <source>about the app</source>
         <translation>o aplikacji</translation>
     </message>
     <message>
-        <location filename="../src/qml/MainDrawerContent.qml" line="113"/>
+        <location filename="../src/qml/MainDrawerContent.qml" line="126"/>
         <source>close app</source>
         <translation>zamknij program</translation>
     </message>
@@ -162,12 +162,17 @@ Place, Fifth Floor, Boston, MA  02110-1301  USA</translation>
         <translation>MetronomeK</translation>
     </message>
     <message>
-        <location filename="../src/qml/MainWindow.qml" line="204"/>
+        <location filename="../src/qml/MainWindow.qml" line="218"/>
         <source>Tap tempo</source>
         <translation>Wystukaj tempo</translation>
     </message>
     <message>
-        <location filename="../src/qml/MainWindow.qml" line="235"/>
+        <location filename="../src/qml/MainWindow.qml" line="218"/>
+        <source>Tempo changes</source>
+        <translation>Zmiany tempa</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/MainWindow.qml" line="255"/>
         <location filename="../src/qml/MeterDrawer.qml" line="20"/>
         <source>count to</source>
         <translation>licz do</translation>
@@ -182,9 +187,38 @@ Place, Fifth Floor, Boston, MA  02110-1301  USA</translation>
     </message>
 </context>
 <context>
+    <name>NextTempoPop</name>
+    <message>
+        <location filename="../src/qml/NextTempoPop.qml" line="28"/>
+        <source>Next tempo</source>
+        <translation>Następne tempo</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/NextTempoPop.qml" line="38"/>
+        <source>Tap, click or press any key.</source>
+        <translation>Stuknij, kliknij lub naciśnij jakiś klawisz.</translation>
+    </message>
+</context>
+<context>
+    <name>QFileDialog</name>
+    <message>
+        <location filename="../src/qml/TempoPage.qml" line="157"/>
+        <source>Remove</source>
+        <translation>Usuń</translation>
+    </message>
+</context>
+<context>
+    <name>QLineEdit</name>
+    <message>
+        <location filename="../src/qml/TempoPartDelegate.qml" line="57"/>
+        <source>Delete</source>
+        <translation>Usuń</translation>
+    </message>
+</context>
+<context>
     <name>QMessageBox</name>
     <message>
-        <location filename="../src/tglob.cpp" line="139"/>
+        <location filename="../src/tglob.cpp" line="155"/>
         <source>&lt;h3&gt;About Qt&lt;/h3&gt;&lt;p&gt;This program uses Qt version %1.&lt;/p&gt;</source>
         <translation>&lt;h3&gt;O Qt&lt;/h3&gt;&lt;p&gt;Ten program używa modelu Qt w wersji %1.&lt;/p&gt;</translation>
     </message>
@@ -192,67 +226,77 @@ Place, Fifth Floor, Boston, MA  02110-1301  USA</translation>
 <context>
     <name>QPlatformTheme</name>
     <message>
+        <location filename="../src/qml/SettingsPage.qml" line="154"/>
         <source>Cancel</source>
         <translation>Anuluj</translation>
     </message>
     <message>
-      <source>Apply</source>
-      <translation>Zastosuj</translation>
+        <location filename="../src/qml/SettingsPage.qml" line="155"/>
+        <source>Apply</source>
+        <translation>Zastosuj</translation>
     </message>
     <message>
-      <source>OK</source>
-      <translation>OK</translation>
+        <location filename="../src/qml/InfoPage.qml" line="95"/>
+        <location filename="../src/qml/TempoPage.qml" line="102"/>
+        <location filename="../src/qml/TempoPage.qml" line="307"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/TempoPage.qml" line="140"/>
+        <source>Reset</source>
+        <translation>Resetuj</translation>
     </message>
 </context>
 <context>
     <name>RingType</name>
     <message>
-        <location filename="../src/taudioout.cpp" line="343"/>
+        <location filename="../src/taudioout.cpp" line="386"/>
         <source>None</source>
         <translation>Brak</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="343"/>
+        <location filename="../src/taudioout.cpp" line="386"/>
         <source>Bell</source>
         <translation>Dzwonek</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="344"/>
+        <location filename="../src/taudioout.cpp" line="387"/>
         <source>Other bell</source>
         <translation>Inny dzwonek</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="344"/>
+        <location filename="../src/taudioout.cpp" line="387"/>
         <source>Yet another bell</source>
         <translation>Jeszcze inny dzwonek</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="345"/>
+        <location filename="../src/taudioout.cpp" line="388"/>
         <source>Glass</source>
         <translation>Szkło</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="345"/>
+        <location filename="../src/taudioout.cpp" line="388"/>
         <source>Metal sheet</source>
         <translation>Blacha</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="346"/>
+        <location filename="../src/taudioout.cpp" line="389"/>
         <source>Spoon at mug</source>
         <translation>Łyżeczką w kubek</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="346"/>
+        <location filename="../src/taudioout.cpp" line="389"/>
         <source>Guitar harmonic</source>
         <translation>Gitarowy flażolet</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="347"/>
+        <location filename="../src/taudioout.cpp" line="390"/>
         <source>Hi hat</source>
         <translation>Hi hat</translation>
     </message>
     <message>
-        <location filename="../src/taudioout.cpp" line="347"/>
+        <location filename="../src/taudioout.cpp" line="390"/>
         <source>Woodblock</source>
         <translation>Drewniana klepka</translation>
     </message>
@@ -275,19 +319,167 @@ Place, Fifth Floor, Boston, MA  02110-1301  USA</translation>
         <translation>Urządzenie dźwiękowe</translation>
     </message>
     <message>
-        <location filename="../src/qml/SettingsPage.qml" line="170"/>
+        <location filename="../src/qml/SettingsPage.qml" line="167"/>
         <source>keep screen on</source>
         <translation>pozostaw ekran włączony</translation>
     </message>
     <message>
-        <location filename="../src/qml/SettingsPage.qml" line="175"/>
+        <location filename="../src/qml/SettingsPage.qml" line="172"/>
         <source>disable screen rotation</source>
         <translation>blokuj obrót ekranu</translation>
     </message>
     <message>
-        <location filename="../src/qml/SettingsPage.qml" line="180"/>
+        <location filename="../src/qml/SettingsPage.qml" line="177"/>
         <source>use full screen</source>
         <translation>użyj całego ekranu</translation>
+    </message>
+</context>
+<context>
+    <name>TempoEdit</name>
+    <message>
+        <location filename="../src/qml/TempoEdit.qml" line="84"/>
+        <source>Tap tempo&lt;br&gt;or drag&lt;br&gt;a handle</source>
+        <translation>Wystukaj tempo&lt;br&gt;lub ciągnij&lt;br&gt;za uchwyt</translation>
+    </message>
+</context>
+<context>
+    <name>TempoPage</name>
+    <message>
+        <location filename="../src/qml/TempoPage.qml" line="40"/>
+        <source>Rhythmic Composition</source>
+        <translation>Kompozycja rytmiczna</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/TempoPage.qml" line="84"/>
+        <source>Add tempo change</source>
+        <translation>Dodaj zmianę tempa</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/TempoPage.qml" line="92"/>
+        <source>Tap or click to edit tempo change.</source>
+        <translation>Stuknij lub kliknij żeby edytować zmianę tempa.</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/TempoPage.qml" line="93"/>
+        <source>Drag the item left or right to remove it.</source>
+        <translation>Przeciągnij element w prawo lub w lewo żeby go usunąć.</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/TempoPage.qml" line="103"/>
+        <source>Actions</source>
+        <translation>Działania</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/TempoPage.qml" line="120"/>
+        <source>New composition</source>
+        <translation>Nowa kompozycja</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/TempoPage.qml" line="127"/>
+        <source>Duplicate</source>
+        <translation>Powiel</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/TempoPage.qml" line="201"/>
+        <source>initial tempo</source>
+        <translation>tempo początkowe</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/TempoPage.qml" line="208"/>
+        <source>target tempo</source>
+        <translation>tempo docelowe</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/TempoPage.qml" line="219"/>
+        <source>Meter</source>
+        <translation>Metrum</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/TempoPage.qml" line="236"/>
+        <source>Duration</source>
+        <translation>Czas</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/TempoPage.qml" line="245"/>
+        <source>infinite</source>
+        <translation>w nieskończoność</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/qml/TempoPage.qml" line="266"/>
+        <source>bars</source>
+        <translation>
+            <numerusform>takt</numerusform>
+            <numerusform>takty</numerusform>
+            <numerusform>taktów</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/qml/TempoPage.qml" line="282"/>
+        <source>beats</source>
+        <translation>
+            <numerusform>uderzenie</numerusform>
+            <numerusform>uderzenia</numerusform>
+            <numerusform>uderzeń</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/qml/TempoPage.qml" line="298"/>
+        <source>seconds</source>
+        <translation>
+            <numerusform>sekunda</numerusform>
+            <numerusform>sekundy</numerusform>
+            <numerusform>sekund</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>TempoPartDelegate</name>
+    <message numerus="yes">
+        <location filename="../src/qml/TempoPartDelegate.qml" line="46"/>
+        <source>&lt;b&gt;%n&lt;/b&gt; bars</source>
+        <translation>
+            <numerusform>&lt;b&gt;%n&lt;/b&gt; takt</numerusform>
+            <numerusform>&lt;b&gt;%n&lt;/b&gt; takty</numerusform>
+            <numerusform>&lt;b&gt;%n&lt;/b&gt; taktów</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/qml/TempoPartDelegate.qml" line="47"/>
+        <source>&lt;b&gt;%n&lt;/b&gt; beats</source>
+        <translation>
+            <numerusform>&lt;b&gt;%n&lt;/b&gt; uderzenie</numerusform>
+            <numerusform>&lt;b&gt;%n&lt;/b&gt; uderzenia</numerusform>
+            <numerusform>&lt;b&gt;%n&lt;/b&gt; uderzeń</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/qml/TempoPartDelegate.qml" line="48"/>
+        <source>&lt;b&gt;%n&lt;/b&gt; seconds</source>
+        <translation>
+            <numerusform>&lt;b&gt;%n&lt;/b&gt; sekunda</numerusform>
+            <numerusform>&lt;b&gt;%n&lt;/b&gt; sekundy</numerusform>
+            <numerusform>&lt;b&gt;%n&lt;/b&gt; sekund</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>TtempoPart</name>
+    <message>
+        <location filename="../src/ttempopart.cpp" line="141"/>
+        <source>accelerando</source>
+        <comment>This is official, glob wide music term, so it shouldn&apos;t be translated.</comment>
+        <translation>accelerando</translation>
+    </message>
+    <message>
+        <location filename="../src/ttempopart.cpp" line="143"/>
+        <source>rallentando</source>
+        <comment>This is official, glob wide music term, so it shouldn&apos;t be translated.</comment>
+        <translation>rallentando</translation>
+    </message>
+    <message>
+        <location filename="../src/ttempopart.cpp" line="147"/>
+        <source>Tempo</source>
+        <translation>Tempo</translation>
     </message>
 </context>
 </TS>
