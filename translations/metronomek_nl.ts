c25f9da3d2ab7ac79009bbb6e2f7f58a58f3d3b8
Heimen Stoffels <vistausss@fastmail.com>
2024-02-14 22:19:23 +0100

    Added Dutch translation

---
+++ translations/metronomek_nl.ts
@@ -0,0 +1,295 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>BeatType</name>
+    <message>
+        <location filename="../src/taudioout.cpp" line="304"/>
+        <source>Metronome beat</source>
+        <translation>Metronoomritme</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="304"/>
+        <source>Metronome beat 2</source>
+        <translation>Metronoomritme 2</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="305"/>
+        <source>Snapping fingers</source>
+        <translation>Knippende vingers</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="305"/>
+        <source>Beating at parapet</source>
+        <translation>Ritme tegen borstwering</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="306"/>
+        <source>Drum sticks</source>
+        <translation>Drumsticks</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="306"/>
+        <source>Drum sticks 2</source>
+        <translation>Drumsticks 2</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="307"/>
+        <source>Clapping</source>
+        <translation>Klappen</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="307"/>
+        <source>Guitar body</source>
+        <translation>Gitaarhals</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="308"/>
+        <source>Drum 1</source>
+        <translation>Drum 1</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="308"/>
+        <source>Drum 2</source>
+        <translation>Drum 2</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="308"/>
+        <source>Drum 3</source>
+        <translation>Drum 3</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="309"/>
+        <source>Base Drum</source>
+        <translation>Basdrum</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="309"/>
+        <source>Snare drum</source>
+        <translation>Kleine trom</translation>
+    </message>
+</context>
+<context>
+    <name>InfoPage</name>
+    <message>
+        <location filename="../src/qml/InfoPage.qml" line="50"/>
+        <source>Metronomek ticks and rings through&lt;br&gt;&lt;b&gt;%1&lt;/b&gt; library.</source>
+        <translation>Metronomek tikt en belt met behulp van de &lt;br&gt;&lt;b&gt;%1&lt;/b&gt;-bibliotheek.</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/InfoPage.qml" line="62"/>
+        <source>This program is free software; you can redistribute it and/or modify
+          it under the terms of the GNU General Public License as published by
+          the Free Software Foundation; either version 3 of the License, or
+          (at your option) any later version.&lt;br&gt;&lt;br&gt;
+
+          This program is distributed in the hope that it will be useful,
+          but WITHOUT ANY WARRANTY; without even the implied warranty of
+          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+          GNU General Public License for more details.&lt;br&gt;&lt;br&gt;
+
+          You should have received a copy of the GNU General Public License
+          along with this program; if not, write to the Free Software Foundation,
+          Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA&lt;br&gt;&lt;br&gt;&lt;a href=&quot;https://www.gnu.org/licenses/gpl-3.0.html&quot;&gt;https://www.gnu.org/licenses/gpl-3.0.html&lt;/a&gt;&lt;br&gt;&lt;br&gt;</source>
+        <translation>This program is free software; you can redistribute it and/or modify
+          it under the terms of the GNU General Public License as published by
+          the Free Software Foundation; either version 3 of the License, or
+          (at your option) any later version.&lt;br&gt;&lt;br&gt;
+
+          This program is distributed in the hope that it will be useful,
+          but WITHOUT ANY WARRANTY; without even the implied warranty of
+          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+          GNU General Public License for more details.&lt;br&gt;&lt;br&gt;
+
+          You should have received a copy of the GNU General Public License
+          along with this program; if not, write to the Free Software Foundation,
+          Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA&lt;br&gt;&lt;br&gt;&lt;a href=&quot;https://www.gnu.org/licenses/gpl-3.0.html&quot;&gt;https://www.gnu.org/licenses/gpl-3.0.html&lt;/a&gt;&lt;br&gt;&lt;br&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>Logo</name>
+    <message>
+        <location filename="../src/qml/Logo.qml" line="47"/>
+        <source>The rhythmic&lt;br&gt;perfection</source>
+        <translation>Ritmische&lt;br&gt;perfectie</translation>
+    </message>
+</context>
+<context>
+    <name>MainDrawerContent</name>
+    <message>
+        <location filename="../src/qml/MainDrawerContent.qml" line="28"/>
+        <source>beat sound</source>
+        <translation>Ritmegeluid</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/MainDrawerContent.qml" line="47"/>
+        <location filename="../src/qml/MainDrawerContent.qml" line="73"/>
+        <source>ring at &quot;one&quot;</source>
+        <translation>Bellen bij ‘één’</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/MainDrawerContent.qml" line="66"/>
+        <source>count down visible</source>
+        <translation>Zichtbare aftelklok</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/MainDrawerContent.qml" line="80"/>
+        <source>pendulum stationary</source>
+        <translation>Stationaire pendule</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/MainDrawerContent.qml" line="87"/>
+        <source>settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/MainDrawerContent.qml" line="97"/>
+        <source>about the app</source>
+        <translation>Over dit programma</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/MainDrawerContent.qml" line="113"/>
+        <source>close app</source>
+        <translation>Programma afsluiten</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/qml/MainWindow.qml" line="21"/>
+        <source>MetronomeK</source>
+        <translation>MetronomeK</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/MainWindow.qml" line="204"/>
+        <source>Tap tempo</source>
+        <translation>Tiktempo</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/MainWindow.qml" line="235"/>
+        <location filename="../src/qml/MeterDrawer.qml" line="20"/>
+        <source>count to</source>
+        <translation>Aftellen naar</translation>
+    </message>
+</context>
+<context>
+    <name>MeterDrawer</name>
+    <message>
+        <location filename="../src/qml/MeterDrawer.qml" line="30"/>
+        <source>none</source>
+        <translation>geen</translation>
+    </message>
+</context>
+<context>
+    <name>QMessageBox</name>
+    <message>
+        <location filename="../src/tglob.cpp" line="137"/>
+        <source>&lt;h3&gt;About Qt&lt;/h3&gt;&lt;p&gt;This program uses Qt version %1.&lt;/p&gt;</source>
+        <translation>&lt;h3&gt;Over Qt&lt;/h3&gt;&lt;p&gt;Dit programma maakt gebruik van Qt-versie %1.&lt;/p&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>QPlatformTheme</name>
+    <message>
+        <location filename="../src/qml/SettingsPage.qml" line="154"/>
+        <source>Cancel</source>
+        <translation>Annuleren</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPage.qml" line="155"/>
+        <source>Apply</source>
+        <translation>Toepassen</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/InfoPage.qml" line="92"/>
+        <source>OK</source>
+        <translation>Oké</translation>
+    </message>
+</context>
+<context>
+    <name>RingType</name>
+    <message>
+        <location filename="../src/taudioout.cpp" line="343"/>
+        <source>None</source>
+        <translation>Niets</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="343"/>
+        <source>Bell</source>
+        <translation>Bel</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="344"/>
+        <source>Other bell</source>
+        <translation>Andere bel</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="344"/>
+        <source>Yet another bell</source>
+        <translation>Nog een andere bel</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="345"/>
+        <source>Glass</source>
+        <translation>Glas</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="345"/>
+        <source>Metal sheet</source>
+        <translation>Metalen plaat</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="346"/>
+        <source>Spoon at mug</source>
+        <translation>Lepel tegen kopje</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="346"/>
+        <source>Guitar harmonic</source>
+        <translation>Gitaarharmonie</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="347"/>
+        <source>Hi hat</source>
+        <translation>Hihat</translation>
+    </message>
+    <message>
+        <location filename="../src/taudioout.cpp" line="347"/>
+        <source>Woodblock</source>
+        <translation>Houten blok</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../src/qml/SettingsPage.qml" line="32"/>
+        <source>default</source>
+        <translation>Standaard</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPage.qml" line="97"/>
+        <source>Language change requires restarting the application!</source>
+        <translation>Herstart het programma om de wijziging toe te passen!</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPage.qml" line="108"/>
+        <source>Sound device</source>
+        <translation>Audio-apparaat</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPage.qml" line="172"/>
+        <source>keep screen on</source>
+        <translation>Scherm niet uitschakelen</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPage.qml" line="177"/>
+        <source>disable screen rotation</source>
+        <translation>Standaard draaiing van scherm</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPage.qml" line="182"/>
+        <source>use full screen</source>
+        <translation>Schermvullende weergave gebruiken</translation>
+    </message>
+</context>
+</TS>
