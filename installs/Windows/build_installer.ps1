858fc407f59e31a52f06c5dc4c45e208f2b04dde
SeeLook <seelook@gmail.com>
2025-09-20 18:45:05 +0200

    WIN: cleared script

--- installs/Windows/build_installer.ps1
+++ installs/Windows/build_installer.ps1
@@ -3,7 +3,6 @@
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 scoop bucket add extras
 scoop install nsis
-# scoop install wget
 
 $mainDir = Get-Location
 echo $mainDir
@@ -17,11 +16,6 @@ cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=ins
 mingw32-make -j4
 
 mingw32-make install
-# mingw32-make deploy
-
-# echo "--- Getting openSSL MinGw_x86 libraries"
-# wget -q "https://www.opencode.net/seelook/metronomek/-/raw/master/spare_parts/openSSL-win32/openSSL-win32.tar.gz"
-# tar -xzf .\openSSL-win32.tar.gz -C .\installs
 
 echo "--- Building installer"
 makensis installs/bin/metronomek.nsi
