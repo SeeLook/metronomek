994f01bca545ff440cb89b03aac80d7854f4e873
SeeLook <seelook@gmail.com>
2025-09-20 09:53:40 +0200

    [Win] adjust install target to Qt 6

--- installs/Windows/build_installer.ps1
+++ installs/Windows/build_installer.ps1
@@ -3,7 +3,7 @@
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 scoop bucket add extras
 scoop install nsis
-scoop install wget
+# scoop install wget
 
 $mainDir = Get-Location
 echo $mainDir
@@ -17,13 +17,13 @@ mingw32-make -j2
 
 mkdir installs
 mingw32-make install
-mingw32-make deploy
+# mingw32-make deploy
 
-echo "--- Getting openSSL MinGw_x86 libraries"
-wget -q "https://www.opencode.net/seelook/metronomek/-/raw/master/spare_parts/openSSL-win32/openSSL-win32.tar.gz"
-tar -xzf .\openSSL-win32.tar.gz -C .\installs
+# echo "--- Getting openSSL MinGw_x86 libraries"
+# wget -q "https://www.opencode.net/seelook/metronomek/-/raw/master/spare_parts/openSSL-win32/openSSL-win32.tar.gz"
+# tar -xzf .\openSSL-win32.tar.gz -C .\installs
 
 echo "--- Building installer"
-makensis installs/metronomek.nsi
+makensis installs/bin/metronomek.nsi
 
 cd ..
